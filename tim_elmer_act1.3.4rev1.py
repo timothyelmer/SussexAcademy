bc5a736c93d30061159daf1ebc50d6a75f63263e
timothyelmer <timothy.elmer@saas.k12.de.us>
2017-07-13 09:45:17 -0400

    Add files via upload

---
+++ tim_elmer_act1.3.4rev1.py
@@ -0,0 +1,16 @@
+# this program will take an input value and determine if it 
+# is an integer, odd, even, or a multiple of 6.
+def f(x):
+    if int(x) == x:
+       if x % 2 == 0:
+            if x % 5 ==0:
+                print x, 'is a multiple of 5'
+            else:
+                print x, ' is an even number'
+       else:
+            print x, ' is an odd number' 
+    else:
+        print x, ' is not an intager'
+     
+        
+    
